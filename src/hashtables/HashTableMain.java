450363cecbdd90386af5788a75aeafc9b937dd76
Tushar Pingale <tushar.pingale@paynearby.in>
2020-07-26 19:47:01 +0530

    - added Custom hashtable

--- src/hashtables/HashTableMain.java
+++ src/hashtables/HashTableMain.java
@@ -23,5 +23,19 @@ public class HashTableMain {
         CharFinder charFinder = new CharFinder();
         System.out.println(charFinder.findFirstNonRepeatingChar("a green apple"));
         System.out.println(charFinder.findFirstRepeatingChar("a green apple"));
+
+        //1. Chaining -> add chain to linked list
+        //2. Probing -> Linear Probing[hash(key) + i], Quadratic Probing [hash(key) + i]^2 % size
+        //Double Hashing ->
+        //Hash function1 (key) -> key % size
+        //Hash function2 (key) -> prime - (key % prime)
+
+        HashTable hashTable = new HashTable();
+        hashTable.put(6,"A");
+        hashTable.put(8,"B");
+        hashTable.put(11,"C");
+        hashTable.put(6,"A+");
+        hashTable.remove(8);
+        System.out.println(hashTable.get(6));
     }
 }
